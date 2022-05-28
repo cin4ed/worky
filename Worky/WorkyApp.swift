21b73386b650769c13e92a4e47c459e46c8f643b
Kenneth Quintero <kennethgqv@gmail.com>
2022-05-28 16:37:07 -0600

    Initial Commit

---
+++ Worky/WorkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkyApp.swift
+//  Worky
+//
+//  Created by Kenneth Quintero on 28/05/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
