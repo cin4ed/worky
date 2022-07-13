97ef55d2bfe6021cad54933de1f188d5ef54a319
Kenneth Quintero <kennethgqv@gmail.com>
2022-07-13 13:49:22 -0600

    Update CreateWorkspaceView.swift

--- Worky/View/CreateWorkspaceView.swift
+++ Worky/View/CreateWorkspaceView.swift
@@ -11,15 +11,15 @@ import Combine
 struct CreateWorkspaceView: View {
     
     @State private var name: String = ""
+    @State private var emoji: String = "📦"
+    @State private var showingPopover = false
+    private var columns: [GridItem] = Array(repeating: .init(.fixed(30)), count: 8)
+    private var emojis: [Emoji] = Emoji.getAll()
     
     var body: some View {
         VStack(alignment: .center) {
             HStack {
-                Menu("📦") {
-                    Text("Something")
-                    Text("Something 1")
-                }
-                .menuStyle(CustomMenuStyle())
+                ButtonEmoji
                 
                 TextField("Name", text: $name)
                     .textFieldStyle(PlainTextFieldStyle())
@@ -33,46 +33,74 @@ struct CreateWorkspaceView: View {
             HStack {
                 Button(action: createButtonHandler) {
                     Text("Create")
-                        .frame(maxWidth: .infinity, maxHeight: 35)
+                        .frame(maxWidth: .infinity, maxHeight: 32)
                         .background(RoundedRectangle(cornerRadius: 5).fill(Color.blue))
-                }.buttonStyle(PlainButtonStyle())
+                }
 
                 Button(action: cancelButtonHandler) {
                     Text("Cancel")
-                        .frame(maxWidth: .infinity, maxHeight: 35)
+                        .frame(maxWidth: .infinity, maxHeight: 32)
                         .background(RoundedRectangle(cornerRadius: 5).fill(Color.gray))
-                }.buttonStyle(PlainButtonStyle())
+                }
             }
         }
+        .buttonStyle(PlainButtonStyle())
         .frame(width: 250, height: nil)
         .padding()
+        .background(VisualEffect())
+        .onSubmit(createButtonHandler)
+        .onExitCommand(perform: cancelButtonHandler)
+    }
+    
+    private var ButtonEmoji: some View {
+        Button($emoji.wrappedValue) {
+            self.showingPopover = true
+        }
+        .frame(maxWidth: 40, maxHeight: 35)
+        .background(RoundedRectangle(cornerRadius: 5).stroke(Color.gray, lineWidth: 1))
+        .popover(isPresented: $showingPopover) {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: columns) {
+                    ForEach(emojis, id: \.self) {
+                        let currEmoji = $0.emoji
+                        Text(currEmoji)
+                            .font(.largeTitle)
+                            .onTapGesture {
+                                self.emoji = currEmoji
+                                self.showingPopover = false
+                            }
+                    }
+                }
+            }
+            .frame(maxWidth: 500, maxHeight: 300)
+            .padding(10)
+        }
     }
 }
-
-fileprivate struct CustomMenuStyle: MenuStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        Menu(configuration)
-            .frame(width: 50, height: 50)
-    }
-}
-
 
 extension CreateWorkspaceView {
+    
     func createButtonHandler() -> Void {
-        let workspace = Workspace(
-            title: $name.wrappedValue,
-            container: WorkyApp.container
-        )
         
-        Workspace.createDirectory(for: workspace)
-        Workspace.serialize(workspace)
-        Workspace.selectWorkspace(workspace)
+        // Only crate one if there's a name
+        if !$name.wrappedValue.isEmpty {
+            
+            let workspace = Workspace(
+                title: $name.wrappedValue.removeEmojis(),
+                emoji: $emoji.wrappedValue,
+                container: WorkyApp.container
+            )
+            
+            Workspace.createDirectory(for: workspace)
+            Workspace.serialize(workspace)
+            Workspace.selectWorkspace(workspace)
+        }
         
-        AppStatusItemMenuDelegate.newWorkspaceWindow!.close()
+        CreateWorkspaceWindow.shared.close()
     }
     
     func cancelButtonHandler() -> Void {
-        AppStatusItemMenuDelegate.newWorkspaceWindow!.close()
+        CreateWorkspaceWindow.shared.close()
     }
 }
 
@@ -80,4 +108,17 @@ struct CreateWorkspaceView_Previews: PreviewProvider {
     static var previews: some View {
         CreateWorkspaceView()
     }
+}
+
+struct VisualEffect: NSViewRepresentable {
+   func makeNSView(context: Self.Context) -> NSView {
+       let visualEffect =  NSVisualEffectView()
+       visualEffect.blendingMode = .behindWindow
+       visualEffect.state = .active
+       visualEffect.material = .hudWindow
+       visualEffect.wantsLayer = true
+       visualEffect.layer?.cornerRadius = 16
+       return visualEffect
+   }
+   func updateNSView(_ nsView: NSView, context: Context) { }
 }
