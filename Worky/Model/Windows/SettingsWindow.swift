cdd6dc88d2e8a02b22b7353b3ed8af1323fab91a
Kenneth Quintero <kennethgqv@gmail.com>
2022-07-13 13:47:31 -0600

    Create SettingsWindow.swift

---
+++ Worky/Model/Windows/SettingsWindow.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsWindow.swift
+//  Worky
+//
+//  Created by Kenneth Quintero on 13/07/22.
+//
+
+import Foundation
+import SwiftUI
+
+class SettingsWindow: NSWindow {
+    
+    static var shared = SettingsWindow()
+    
+    private init() {
+        super.init(
+            contentRect: NSRect(x: 0, y: 0, width: 800, height: 500),
+            styleMask: [.titled, .fullSizeContentView],
+            backing: .buffered,
+            defer: false)
+        
+        self.title = "Worky Settings"
+        self.isReleasedWhenClosed = false
+        self.contentView = NSHostingView(rootView: SettingsView())
+        self.center()
+    }
+    
+    func bringToFront() {
+        NSApp.activate(ignoringOtherApps: true)
+        self.makeKeyAndOrderFront(nil)
+    }
+}
