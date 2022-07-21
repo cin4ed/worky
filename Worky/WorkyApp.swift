d658ecb498db30461351ec01b2c4c8e19c74a65c
Kenneth Quintero <kennethgqv@gmail.com>
2022-07-21 13:42:05 -0600

    Logging and refactoring

--- Worky/WorkyApp.swift
+++ Worky/WorkyApp.swift
@@ -26,6 +26,7 @@ struct WorkyApp: App {
     }
     
     init() {
+        startupLog.info("Application has started.")
         
         WorkyApp.shared = self
         
@@ -38,29 +39,52 @@ struct WorkyApp: App {
             .appendingPathComponent("Documents/Worky")
         
         // Stop execution if can't create the container dir
-        try! fm.createDirectory(
-            at: containerURL,
-            withIntermediateDirectories: true,
-            attributes: nil)
+        do {
+            startupLog.info("Creating workspace container at $HOME/Documents/Worky")
+            try fm.createDirectory(
+                at: containerURL,
+                withIntermediateDirectories: true,
+                attributes: nil)
+        } catch {
+            startupLog.error("Could not create workspace container at $HOME/Documents/Worky. Error: \(error.localizedDescription, privacy: .public)")
+            fatalError("Couldn't create workspace container at Document/Worky. Error: \(error)")
+        }
         
         // If everything went well set the container
+        startupLog.info("Setting the container to default location $HOME/Documens/Worky")
         WorkyApp.container = containerURL
         
         // Check if there's a workspace in the desktop directory,
         // if so, set current workspace to this
+        startupLog.info("Checking wheter there is a workspace in desktop already.")
         let desktopURL = fm
             .homeDirectoryForCurrentUser
             .appendingPathComponent("Desktop")
         
         // Trying to read the contents of desktop this shouldn't go
         // wrong right? so force unwrap
-        let desktopContents = try! fm.contentsOfDirectory(
-            at: desktopURL, includingPropertiesForKeys: nil)
-         
+        guard let desktopContents = try? fm.contentsOfDirectory(
+            at: desktopURL,
+            includingPropertiesForKeys: nil)
+        else {
+            startupLog.critical("Could not get the contents of desktop.")
+            fatalError("Could not get the contents of desktop.")
+        }
+        
         for file in desktopContents {
             if file.path.contains(".worky.json") {
-                let workspace = try! Workspace(file: file)
+                startupLog.info("Workspace found in desktop. Trying to instantiate it.")
+                
+                guard let workspace = try? Workspace(file: file) else {
+                    startupLog.critical("Could not instantiate workspace found in desktop.")
+                    fatalError("Could not instantiate workspace found in desktop at startup.")
+                }
+                
+                startupLog.info("Workspace encountered in desktop. \(workspace.title, privacy: .public)")
+                
+                startupLog.info("Setting current workspace to workspace found in desktop.")
                 WorkyApp.currentWorkspace = workspace
+                
                 break
             }
         }
