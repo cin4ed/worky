18eb26b6a088521d14c254bedf41bb00411acf7e
Kenneth Quintero <kennethgqv@gmail.com>
2022-07-21 14:12:07 -0600

    Add logging

--- Worky/Model/AppleScriptExecutor.swift
+++ Worky/Model/AppleScriptExecutor.swift
@@ -11,32 +11,49 @@ struct AppleScriptExecutor {
     
     @discardableResult
     static func toggleDesktopVisibility() -> (success: Bool, error: NSDictionary?) {
+        appleScriptLog.info("Toggle desktop visibility triggered.")
         
+        appleScriptLog.info("Getting script file from bundle.")
         guard let scriptFilePath = Bundle.main.path(
             forResource: "ToggleDesktopVisibility",
             ofType: "scpt")
         else {
-            print("apple script file not found")
+            appleScriptLog.error("Could not find ToggleDesktopVisibility.scpt in Bundle.")
             return (false, nil)
         }
+        appleScriptLog.info("Script file found and returned succesfully.")
         
-        let contentOfFile = try? String(contentsOfFile: scriptFilePath)
-    
-        guard let appleScript = contentOfFile else {
+//        let contentOfFile = try? String(contentsOfFile: scriptFilePath)
+//
+//        guard let appleScript = contentOfFile else {
+//            return (false, nil)
+//        }
+        
+        appleScriptLog.info("Trying to read the contents of the script file.")
+        guard let appleScript = try? String(contentsOfFile: scriptFilePath) else {
+            appleScriptLog.error("Could not read the contents of the script file.")
             return (false, nil)
         }
+        appleScriptLog.info("Contents read succesfully.")
         
         var error: NSDictionary?
         
+        appleScriptLog.info("Trying to instantiate NSAppleScript with script file.")
         if let scriptObject = NSAppleScript(source: appleScript) {
+            appleScriptLog.info("Instantiation successfully.")
+            
+            appleScriptLog.info("Executing script.")
             let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error)
+            appleScriptLog.info("Script executed succesfully.")
             
             if error == nil {
                 return (output.booleanValue, nil)}
             else {
                 return (false, error)}
+        } else {
+            
+            appleScriptLog.error("Could not instantiate NSAppleScript with script file.")
+            return (false, error)
         }
-        
-        return (false, error)
     }
 }
