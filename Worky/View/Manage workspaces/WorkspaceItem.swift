bcd22cebc764b7f79dc60705052cf3d4b6640c00
Kenneth Quintero <kennethgqv@gmail.com>
2022-08-05 00:39:26 -0600

    Add: Export workspace feature

--- Worky/View/Manage workspaces/WorkspaceItem.swift
+++ Worky/View/Manage workspaces/WorkspaceItem.swift
@@ -11,12 +11,37 @@ struct WorkspaceItem: View {
     @EnvironmentObject var worky: WorkyModel
     @State var workspace: Workspace
     @State var presentingAlert = false
+    @State var overExport = false
     
     var body: some View {
         HStack {
             Text(workspace.emoji)
             Text(workspace.title)
+            
             Spacer()
+            
+            // Export button
+            Button {
+                // Export: shows the workspace directory in finder
+                NSWorkspace.shared.selectFile(nil, inFileViewerRootedAtPath: workspace.url.path)
+            } label: {
+                Image(systemName: "square.and.arrow.up.fill")
+            }
+            .help("Export Workspace")
+            .buttonStyle(PlainButtonStyle())
+            .onHover { over in overExport = over }
+            .background {
+                if overExport {
+                    Rectangle()
+                        .fill(Color(.sRGB, red: 255, green: 255, blue: 255, opacity: 0.2))
+                        .cornerRadius(2.5)
+                        .frame(width: 20, height: 20)
+                } else {
+                    EmptyView()
+                }
+            }
+
+            // Delete button
             Button { presentingAlert = true } label: {
                 Image(systemName: "trash.fill")
             }
