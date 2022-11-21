f583c41ba2175d4dd8947420d1eefa9df7435678
Kenneth Quintero <kennethgqv@gmail.com>
2022-11-21 09:58:53 -0700

    Create SystemState.swift

---
+++ Worky/SystemState.swift
@@ -0,0 +1,19 @@
+//
+//  SystemState.swift
+//  Worky
+//
+//  Created by Kenneth Quintero on 21/11/22.
+//
+
+import Foundation
+
+enum SystemState {
+    case _000
+    case _001
+    case _010
+    case _011
+    case _100
+    case _101
+    case _110
+    case _111
+}
